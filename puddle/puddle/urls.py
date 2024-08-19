7fc9acc887ee164f84c0bab68b9af4f28b8a11ef
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-20 01:00:19 +0300

    New changes

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -24,12 +24,3 @@ urlpatterns = [
     path('contact/', contact, name='contact'),  # Ensure this line is present
     path('admin/', admin.site.urls),
 ]
-
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('contact/', views.contact, name='contact'),  # Ensure this line is present
-#     path('admin/', admin.site.urls),
-# ]
