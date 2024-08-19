db042af90c0b5901f5087e639d90dadc558aad2e
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-20 01:39:56 +0300

    New changes

--- puddle/core/models.py
+++ puddle/core/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User
-from django.db import models
+# from django.contrib.auth.models import User
+# from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
+# class Category(models.Model):
+#     name = models.CharField(max_length=255)
     
 #     class Meta:
 #         ordering = ('name',)
--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
