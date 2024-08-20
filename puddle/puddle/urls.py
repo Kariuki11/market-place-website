150ddb455d95f1e919ed4b1313b2ffe8284f244c
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-20 14:44:17 +0300

    New changes

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -31,12 +31,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
