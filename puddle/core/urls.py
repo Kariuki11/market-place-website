384461f110e844f0300e3f878523bc12e8426e58
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-20 16:46:03 +0300

    "Updated templates, views, and URLs for core app, including new signup feature"

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -6,6 +6,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup')
 ]
