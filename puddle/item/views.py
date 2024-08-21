3712b84688455acedb91c4334367886bdf436b2f
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-21 20:44:05 +0300

    "Updated views.py to add search functionality, modified items.html template to include search form and hr element, and updated pycache files."

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm, EditItemForm  # Corrected import
-from .models import Item
+from .models import Category, Item
 
 def items(request):
+    query = request.GET.get('query', '')
+    categories = Category.objects.all()
     items = Item.objects.filter(is_sold=False)
+    
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
     
     return render(request, 'item/items.html', {
         'items': items,
+        'query': query,
+        'categories': categories,
     })
 
 def detail(request, pk):
