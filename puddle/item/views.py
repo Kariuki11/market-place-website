08e3c0dc340c88012929f65d293e6368d3ff9141
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-21 20:14:17 +0300

    "Updated base.html template, added 'items' URL and view, and modified db.sqlite3 and __pycache__ files."

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewItemForm, EditItemForm  # Corrected import
 from .models import Item
 
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    
+    return render(request, 'item/items.html', {
+        'items': items,
+    })
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
