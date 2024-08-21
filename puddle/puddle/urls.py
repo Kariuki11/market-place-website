845cb5c78a00733aaebfccc14a50797604cfa1db
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-21 18:41:17 +0300

    "Updated base.html template, added dashboard view and URL, and modified project URLs"

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -36,6 +36,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
 
