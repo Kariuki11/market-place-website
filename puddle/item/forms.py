1c5178f66854ce93c39d1a312eb43fb5656d2add
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-21 19:53:50 +0300

    "Updated item forms, views, and templates to include 'is_sold' field and added edit functionality"

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -30,7 +30,7 @@ class NewItemForm(forms.ModelForm):
 class EditItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'description', 'price', 'image']
+        fields = ['name', 'description', 'price', 'image', 'is_sold']
         
         widgets = {
             'name': forms.TextInput(attrs={
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm  # Corrected import
+from .forms import NewItemForm, EditItemForm  # Corrected import
 from .models import Item
 
 def detail(request, pk):
@@ -30,6 +30,25 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New item'
+    })
+    
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item'
     })
     
 @login_required
